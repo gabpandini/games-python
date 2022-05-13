8795419f72f1e22a95286b1c9ceb251bee254e4c
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2022-05-13 19:10:24 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,10 @@
+
+def jogar():
+    print("Boas vindas ao jogo de Forca!")
+    print("***********************************")
+
+    print("***********")
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
